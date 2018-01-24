7350e2f61968c509d469eb0dd28f1690e05a152f
fishyimm <sedthasith.r@gmail.com>
2018-01-24 22:40:33 +0700

    - add user's post models - add user's post git hub service with params - add function to get user's post

--- app/src/main/java/com/example/application/testretrofitrestapi/MainActivity.java
+++ app/src/main/java/com/example/application/testretrofitrestapi/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.application.testretrofitrestapi.interfaces.GitHubService;
+import com.example.application.testretrofitrestapi.models.Post;
 import com.example.application.testretrofitrestapi.models.User;
 
 import java.util.List;
@@ -24,9 +25,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    GitHubService gitHubService;
     String baseUrl = "https://jsonplaceholder.typicode.com/";
 
+//    https://stackoverflow.com/questions/24100372/retrofit-and-get-using-parameters
 //    https://inthecheesefactory.com/blog/retrofit-2.0/th
 //    https://medium.com/@anirut.311/%E0%B8%A1%E0%B8%B2%E0%B9%80%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A1%E0%B8%95%E0%B9%88%E0%B8%AD-network-%E0%B8%94%E0%B9%89%E0%B8%A7%E0%B8%A2-retrofit2-%E0%B8%81%E0%B8%B1%E0%B8%99%E0%B9%80%E0%B8%96%E0%B8%AD%E0%B8%B0-c1a004e6052e
     @Override
@@ -41,38 +42,74 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                new HttpRequestTask().execute();
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(baseUrl)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
 
-                GitHubService gitHubService = retrofit.create(GitHubService.class);
-                Call<List<User>> call = gitHubService.getUsers();
-                call.enqueue(new Callback<List<User>>() {
-                    @Override
-                    public void onResponse(Call<List<User>> call, Response<List<User>> response) {
-                        Log.i("onResponse", String.valueOf(response.body().size()) );
-                        Log.i("onResponse", response.body().toString() );
-                        if(response.body().size() > 0) {
-                            for(User user : response.body()) {
-                                Log.i("getname", user.getName().toString() );
-                                Log.i("getWebsite", user.getWebsite().toString() );
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call call, Throwable t) {
-                        Log.i("onFailure", call.toString());
-                    }
-                });
-
+                getUsersPost();
+//                getUsers();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
 
+    public void getUsersPost() {
+        Retrofit retrofit  = getRetrofitInstance();
+        GitHubService gitHubService = retrofit.create(GitHubService.class);
+
+        Call<List<Post>> call = gitHubService.getUsersPost("1");
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                Log.i("onResponse", String.valueOf(response.body().size()) );
+                Log.i("onResponse", response.body().toString() );
+                if(response.body().size() > 0) {
+                    for(Post post : response.body()) {
+                        Log.i("getId", post.getId().toString() );
+                        Log.i("getTitle", post.getTitle().toString() );
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.i("onFailure", call.toString());
+            }
+        });
+
+    }
+
+    public void getUsers() {
+        Retrofit retrofit  = getRetrofitInstance();
+
+        GitHubService gitHubService = retrofit.create(GitHubService.class);
+        Call<List<User>> call = gitHubService.getUsers();
+        call.enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                Log.i("onResponse", String.valueOf(response.body().size()) );
+                Log.i("onResponse", response.body().toString() );
+                if(response.body().size() > 0) {
+                    for(User user : response.body()) {
+                        Log.i("getname", user.getName().toString() );
+                        Log.i("getWebsite", user.getWebsite().toString() );
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.i("onFailure", call.toString());
+            }
+        });
+    }
+
+    public Retrofit getRetrofitInstance() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        return retrofit;
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/example/application/testretrofitrestapi/interfaces/GitHubService.java
+++ app/src/main/java/com/example/application/testretrofitrestapi/interfaces/GitHubService.java
@@ -1,14 +1,20 @@
 package com.example.application.testretrofitrestapi.interfaces;
 
+import com.example.application.testretrofitrestapi.models.Post;
 import com.example.application.testretrofitrestapi.models.User;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface GitHubService {
 
     @GET("users")
     Call<List<User>> getUsers();
+
+    @GET("posts")
+    Call<List<Post>> getUsersPost(@Query("userId") String userId);
 }
